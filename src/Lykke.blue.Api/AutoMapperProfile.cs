0a997b724e4b8ce2b2e619d8cc201e643cc4c978
Miladin Joksic <miladin.joksic@primeholding.com>
2017-11-06 12:50:53 +0100

    Added AutoMapper mapping for auth requests and responses

--- src/Lykke.blue.Api/AutoMapperProfile.cs
+++ src/Lykke.blue.Api/AutoMapperProfile.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using Lykke.blue.Api.Requests;
+using Lykke.blue.Api.Responses;
+using Lykke.Service.Registration.Models;
 
 namespace Lykke.blue.Api
 {
@@ -6,7 +9,12 @@ namespace Lykke.blue.Api
     {
         public AutoMapperProfile()
         {
-            
+            CreateMap<AuthRequestModel, AuthModel>()
+                .ForMember(dest => dest.UserAgent, opt => opt.Ignore())
+                .ForMember(dest => dest.Ip, opt => opt.Ignore());
+
+            CreateMap<AuthResponse, AuthResponseModel>()
+                .ForMember(dest => dest.AccessToken, opt => opt.MapFrom(src => src.Token));
         }
     }
 }
--- src/Lykke.blue.Api/Controllers/ClientController.cs
+++ src/Lykke.blue.Api/Controllers/ClientController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Threading.Tasks;
+using AutoMapper;
 using Common.Log;
 using Lykke.blue.Api.Credentials;
 using Lykke.blue.Api.Infrastructure;
@@ -36,31 +37,26 @@ namespace Lykke.blue.Api.Controllers
         }
 
         /// <summary>
-        /// Authenticate.
+        /// Authenticate user.
         /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [HttpPost("auth")]
         [SwaggerOperation("Auth")]
         [ProducesResponseType(typeof(AuthResponseModel), (int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(void), (int)HttpStatusCode.BadRequest)]
-        public async Task<IActionResult> Auth([FromBody]AuthRequestModel model)
+        public async Task<IActionResult> Auth([FromBody]AuthRequestModel request)
         {
-            var authResult = await _lykkeRegistrationClient.AuthorizeAsync(new AuthModel
-            {
-                ClientInfo = model.ClientInfo,
-                Email = model.Email,
-                Password = model.Password,
-                Ip = _requestContext.GetIp(),
-                UserAgent = _requestContext.UserAgent,
-                PartnerId = model.PartnerId
-            });
+            var authModel = Mapper.Map<AuthModel>(request);
+            authModel.Ip = _requestContext.GetIp();
+            authModel.UserAgent = _requestContext.UserAgent;
+
+            var authResult = await _lykkeRegistrationClient.AuthorizeAsync(authModel);
 
             if (authResult?.Status == AuthenticationStatus.Error)
                 return BadRequest(new { message = authResult.ErrorMessage });
 
-            return Ok(new AuthResponseModel
-            {
-                AccessToken = authResult?.Token,
-            });
+            return Ok(Mapper.Map<AuthResponseModel>(authResult));
         }
     }
 }
